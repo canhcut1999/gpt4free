dbb69bc24dd4a9ad7cb7be6132d5c7234ad6fd4f
canhcut1999 <127088414+canhcut1999@users.noreply.github.com>
2025-02-04 20:28:08 +0700

    Update vercelai_test.py

--- unfinished/vercelai/vercelai_test.py
+++ unfinished/vercelai/vercelai_test.py
@@ -3,3 +3,36 @@ import vercelai
 for token in vercelai.Completion.create('summarize the gnu gpl 1.0'):
     print(token, end='', flush=True)
 
+
+
+
+
+
+
+
+[if
+
+
+
+
+
+json_data = {
+    'prompt': 'hello\n',
+    'model': 'openai:gpt-3.5-turbo',
+    'temperature': 0.7,
+    'maxTokens': 200,
+    'topK': 1,
+    'topP': 1,
+    'frequencyPenalty': 1,
+    'presencePenalty': 1,
+    'stopSequences': [],
+}
+
+
+
+
+
+
+
+
+]
